d6ac66bd51dc95c5f2459a2397773d2a18a77694
Pablo Ignacio Carrasco <56566414+PabloC-c@users.noreply.github.com>
2023-06-05 11:46:15 -0400

    Update training.py

--- training.py
+++ training.py
@@ -19,12 +19,13 @@ testset = datasets.MNIST(root='./data', train=False, download=True, transform=tr
 testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
 
 # Crear la instancia de la red neuronal
-neuron_list = [50]
+neuron_list = [10,25,50]
 layer_list  = [2,3]
 activation = 'relu'
-for n_layers in layer_list:
-    for n_neurons in neuron_list:
-        if not os.path.exists("model_weights_L{}_n{}.pth".format(n_layers, n_neurons)):
+for n_neurons in neuron_list:
+    for n_layers in layer_list if n_neurons != 50 else [2]:
+        if True:#not os.path.exists(activation + "_" + "model_weights_L{}_n{}.pth".format(n_layers, n_neurons)):
+            print('\n Capas: ',n_layers,' Neuronas: ',n_neurons,'\n')
             net = training(activation,n_layers,n_neurons,trainset,trainloader,testset,testloader)
             # Guardar los parámetros de la red
             torch.save(net.state_dict(), activation + "_" + "model_weights_L{}_n{}.pth".format(n_layers, n_neurons)) 
