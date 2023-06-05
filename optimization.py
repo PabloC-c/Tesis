c6b81fd19853067baf140ebe0f37eb530b09bca6
Pablo Ignacio Carrasco <56566414+PabloC-c@users.noreply.github.com>
2023-06-05 11:46:58 -0400

    Update optimization.py

--- optimization.py
+++ optimization.py
@@ -7,41 +7,23 @@ from functions import *
 neuron_list = [10,25,50]
 layer_list  = [2,3]
 activation= 'softplus'
+filter_tol = 1e-5
+print('Tolerancia del filtro: ',filter_tol)
 data = OrderedDict()
 
 for n_neurons in neuron_list:
     for n_layers in layer_list if n_neurons != 50 else [2]:
         print('\n Capas: ',n_layers,' Neuronas: ',n_neurons,'\n')
-        ##
-        if activation == 'relu':
-            ## Crear la instancia de la red neuronal
-            net = relu_net(n_neurons,n_layers)
-    
-            ## Cargar los parámetros de la red
-            net.load_state_dict(torch.load(activation+"_"+"model_weights_L{}_n{}.pth".format(n_layers, n_neurons)))
-            
-            ## Guardar los parametos de la red
-            params = net.state_dict()
-                        
-            filtered_params = filter_params(params)
-            
-            bounds,layer_times = calculate_bounds(filtered_params)
-            
-            avg_width,stables = analysis_bounds(bounds)
-                
-        else:
-            ## Crear la instancia de la red neuronal
-            net = nl_net(activation,n_neurons,n_layers)
-    
-            ## Cargar los parámetros de la red
-            net.load_state_dict(torch.load(activation+"_"+"model_weights_L{}_n{}.pth".format(n_layers, n_neurons)))
-        
-            ## Guardar los parametos de la red
-            params = net.state_dict()
-            
-            filtered_params = filter_params(params)    
-            bounds,layer_times = calculate_bounds_no_linear_nl(filtered_params,activation)
-        
-            avg_width,stables = analysis_bounds(bounds)
-            
-        data[(n_layers,n_neurons)] = [bounds,layer_times,avg_width,stables]
+        ## Crear la instancia de la red neuronal
+        net = relu_net(n_neurons,n_layers)
+        ## Cargar los parámetros de la red
+        net.load_state_dict(torch.load(activation+"_"+"model_weights_L{}_n{}.pth".format(n_layers, n_neurons)))
+        ## Guardar los parametos de la red
+        params = net.state_dict()
+        filtered_params = filter_params(params,filter_tol)
+        bounds,layers_time,net_model,input_var,output_var = calculate_bounds(filtered_params,activation)
+        avg_width,stables = analysis_bounds(bounds)
+        data[(n_layers,n_neurons)] = [bounds,layers_time,avg_width,stables]
+        print('Tiempos: ',layers_time)
+        print('Tamaños: ',avg_width)
+        print('Estables: ',sum(stables))
