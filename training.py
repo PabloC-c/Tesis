615b08d3740c954035facb15f3f97e9ce74f2cba
Pablo Ignacio Carrasco <56566414+PabloC-c@users.noreply.github.com>
2023-06-12 11:31:52 -0400

    Update training.py

--- training.py
+++ training.py
@@ -7,21 +7,20 @@ from functions import *
 
 # Transformación para normalizar los datos
 transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,))
+    transforms.ToTensor()
 ])
 
 # Cargar los datos de entrenamiento y validación de MNIST
 trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=100, shuffle=True)
 
 testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=True)
 
 # Crear la instancia de la red neuronal
 neuron_list = [10,25,50]
 layer_list  = [2,3]
-activation = 'relu'
+activation = 'softplus'
 for n_neurons in neuron_list:
     for n_layers in layer_list if n_neurons != 50 else [2]:
         if True:#not os.path.exists(activation + "_" + "model_weights_L{}_n{}.pth".format(n_layers, n_neurons)):
