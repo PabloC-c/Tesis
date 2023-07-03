64dbdeaf1d79984f9aa22c24b1a648c21d2db3a0
Pablo Ignacio Carrasco <56566414+PabloC-c@users.noreply.github.com>
2023-07-03 11:17:50 -0400

    Update optimization.py

--- optimization.py
+++ optimization.py
@@ -1,18 +1,20 @@
 import torch
 import torch.nn as nn
+import pandas as pd
 from pyscipopt import Model,quicksum
 from collections import OrderedDict
 from functions import *
                 
-neuron_list = [10,25,50,75]
-layer_list  = [2,3]
+neuron_list = [10]
+layer_list  = [3]
 activation= 'softplus'
 filter_tol = 1e-3
 print('Tolerancia del filtro: ',filter_tol)
 data = OrderedDict()
+df = pd.DataFrame()
 
 for n_neurons in neuron_list:
-    for n_layers in layer_list: #if n_neurons != 50 else [2]:
+    for n_layers in layer_list if n_neurons != 50 else [2]:
         print('\n Capas: ',n_layers,' Neuronas: ',n_neurons,'\n')
         ## Crear la instancia de la red neuronal
         net = neural_network(n_neurons,n_layers)
@@ -27,3 +29,9 @@ for n_neurons in neuron_list:
         print('Tiempos: ',layers_time)
         print('Tamaños: ',avg_width)
         print('Estables: ',sum(stables))
+        if n_layers == 2:
+            nueva_fila = [n_neurons,0.005,n_layers,layers_time[0],layers_time[1],'-',avg_width[0],avg_width[1],'-',sum(stables)]
+        else:
+            nueva_fila = [n_neurons,0.005,n_layers,layers_time[0],layers_time[1],layers_time[2],avg_width[0],avg_width[1],avg_width[2],sum(stables)]
+        df = df.append(pd.Series(nueva_fila), ignore_index=True)
+df.to_excel('datos_{}.xlsx'.format(activation), header = False, index = False)
