880be7a009a65451ee00ca7de83d8d68f6c1d695
Pablo Ignacio Carrasco <56566414+PabloC-c@users.noreply.github.com>
2023-07-10 11:19:52 -0400

    Update verification.py

--- verification.py
+++ verification.py
@@ -1,45 +1,35 @@
 import torch
-import os
-import torch.nn as nn
-import torch.optim as optim
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 from scipy.special import softmax 
 from functions import *
 
-## Transformación para normalizar los datos
-transform = transforms.Compose([
-    transforms.ToTensor()
-])
-
-## Cargar los datos de entrenamiento y validación de MNIST
-trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64)#, shuffle=True)
-
-testset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=64)#, shuffle=True)
+test_dataset = datasets.MNIST(root='./data', train=False, transform=transforms.ToTensor())
+test_loader  = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
 
 ## Obtener un solo par de entrada y salida correspondiente del conjunto de datos de entrenamiento
-input_example, output_example = next(iter(trainloader))
+input_example, output_example = next(iter(test_loader))
 
 ## Acceder al tensor de entrada con los valores de los píxeles
 choosen_digit = 1
 input_value = input_example[output_example == choosen_digit][0].view(-1,784).tolist()[0]#input_example[0].view(-1,784).tolist()[0]
 image_input = np.array(input_value).reshape(28, 28)
-output_target = 9
+output_target = 7
 
 ## Crear la instancia de la red neuronal
 n_neurons = 10
 n_layers = 2
 activation = 'relu'
+exact = 'no_exact' #prop: propagacion, exact: exacto, no_exact: formulaciones o envolturas
 net = neural_network(n_neurons,n_layers,activation)
 
 ## Cargar los parámetros de la red
-net.load_state_dict(torch.load(activation+"_"+"model_weights_L{}_n{}.pth".format(n_layers, n_neurons)))
+net.load_state_dict(torch.load('nn_parameters/'+activation+"_"+"model_weights_L{}_n{}.pth".format(n_layers, n_neurons)))
 params = net.state_dict()
 filtered_params = filter_params(params)
 
-bounds,layers_time,net_model,net_input_var,net_output_var,all_vars = calculate_bounds(params,activation)
+
+bounds,layers_time,net_model,net_input_var,net_output_var,all_vars = calculate_bounds(params,activation,exact)
 
 #solution = calculate_variables(net_model,input_value,filtered_params,all_vars,activation)
 
@@ -47,7 +37,7 @@ bounds,layers_time,net_model,net_input_var,net_output_var,all_vars = calculate_b
 image = torch.tensor(input_value).view(1, 28, 28)
 aux   = net(image).tolist()[0]
 apply_softmax = False
-tol_distance = 0.209
+tol_distance = 0.2
 if apply_softmax:
     aux   = softmax(np.array(aux))
 output_value = aux[choosen_digit]
