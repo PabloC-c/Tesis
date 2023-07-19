851cbfd0f64bad8126d075806ce08c545499c727
Pablo Ignacio Carrasco <56566414+PabloC-c@users.noreply.github.com>
2023-07-19 11:22:44 -0400

    Update verification.py

--- verification.py
+++ verification.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 from scipy.special import softmax 
@@ -12,15 +13,19 @@ input_example, output_example = next(iter(test_loader))
 
 ## Acceder al tensor de entrada con los valores de los píxeles
 choosen_digit = 1
-input_value = input_example[output_example == choosen_digit][0].view(-1,784).tolist()[0]#input_example[0].view(-1,784).tolist()[0]
-image_input = np.array(input_value).reshape(28, 28)
-output_target = 7
+image_list = input_example[output_example == choosen_digit][0].view(-1,784).tolist()[0]#input_example[0].view(-1,784).tolist()[0]
+image_input = np.array(image_list).reshape(28, 28)
+output_target = 9
 
 ## Crear la instancia de la red neuronal
 n_neurons = 10
 n_layers = 2
 activation = 'relu'
-exact = 'no_exact' #prop: propagacion, exact: exacto, no_exact: formulaciones o envolturas
+exact = 'exact' #prop: propagacion, exact: exacto, no_exact: formulaciones o envolturas
+apply_softmax = False 
+tol_distance = 0.1
+apply_bounds = True
+minutes = 15
 net = neural_network(n_neurons,n_layers,activation)
 
 ## Cargar los parámetros de la red
@@ -28,25 +33,19 @@ net.load_state_dict(torch.load('nn_parameters/'+activation+"_"+"model_weights_L{
 params = net.state_dict()
 filtered_params = filter_params(params)
 
+## Se cargan las cotas de la red
+bounds = read_bounds(n_layers,n_neurons,activation)
 
-bounds,layers_time,net_model,net_input_var,net_output_var,all_vars = calculate_bounds(params,activation,exact)
-
-#solution = calculate_variables(net_model,input_value,filtered_params,all_vars,activation)
-
-#flag = net_model.trySol(solution)
-image = torch.tensor(input_value).view(1, 28, 28)
+image = torch.tensor(image_list).view(1, 28, 28)
 aux   = net(image).tolist()[0]
-apply_softmax = False
-tol_distance = 0.2
-if apply_softmax:
-    aux   = softmax(np.array(aux))
-output_value = aux[choosen_digit]
 
-net_model = create_verification_model(net_model,net_input_var,net_output_var,input_value,choosen_digit,output_value,output_target,params,bounds,tol_distance, apply_softmax)
-net_model.redirectOutput()
-net_model.optimize()
+verif_model,all_vars = create_verification_model(params,bounds,activation,tol_distance,apply_softmax,image_list,output_target,choosen_digit,exact,apply_bounds)
+verif_model.redirectOutput()
+verif_model.setParam('limits/time', int(60*minutes))
+verif_model.optimize()
 
-solution = [net_model.getVal(all_vars['h{},{}'.format(-1,i)]) for i in range(len(input_value))]
+
+solution = [verif_model.getVal(all_vars['h{},{}'.format(-1,i)]) for i in range(len(image_list))]
 image_solution = np.array(solution).reshape(28, 28)
 
 # Crea una figura con dos subplots
