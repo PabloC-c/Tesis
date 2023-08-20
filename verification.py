284298ed504140994330c842948206c8c5875cc5
Pablo Ignacio Carrasco <pablo.carrasco@pregrado.uoh.cl>
2023-08-20 15:52:08 -0400

    files updated

--- verification.py
+++ verification.py
@@ -20,9 +20,7 @@ real_output = 1
 target_output = 7
 input_lb =0 
 input_ub = 1
-tol_0 = 0.01
-tol_f = 0.05
-tol_step = 0.04
+tols_list = [0.01,0.05]
 
 if len(sys.argv) > 1:
     activation_list = [sys.argv[1]]
@@ -60,24 +58,19 @@ for activation in activation_list:
                 file_name = 'verif_results/{}/datos_verificacion_{}_1como7.xlsx'.format(exact,activation)
                 if activation != 'relu' and exact == 'no_exact':
                     break
-                aux_list = apply_bounds_list
-                if exact == 'no_exact':
-                    aux_list = [True]
-                ## Se aplica o no se aplican las cotas
-                for apply_bounds in aux_list:
-                    aux_bounds_list = type_bounds_list
-                    if not apply_bounds:
-                        aux_bounds_list = ['-']
-                    ## Se recorren los tipos de cotas
-                    for type_bounds in aux_bounds_list:
-                        if True:
-                            ## Se crea la instancia de la red neuronal
-                            net = neural_network(n_neurons,n_layers,activation)
-                            ## Se cargan los parámetros de la red
-                            net.load_state_dict(torch.load('nn_parameters/{}_model_weights_L{}_n{}.pth'.format(activation,n_layers, n_neurons)))
-                            params = net.state_dict()
-                            ## Se filtran los parametros
-                            filtered_params = filter_params(params)
+                for tol_distance in tols_list:
+                    ## Lista de info a guardar 
+                    new_line = [n_layers,n_neurons,tol_distance]
+                    aux_list = apply_bounds_list
+                    if exact == 'no_exact':
+                        aux_list = [True]
+                    ## Se aplica o no se aplican las cotas
+                    for apply_bounds in aux_list:
+                        aux_bounds_list = type_bounds_list
+                        if not apply_bounds:
+                            aux_bounds_list = ['-']
+                        ## Se recorren los tipos de cotas
+                        for type_bounds in aux_bounds_list:
                             ## Caso donde se deben aplicar las cotas
                             if apply_bounds:
                                 if type_bounds == 'prop':
@@ -93,75 +86,75 @@ for activation in activation_list:
                                     break
                             else:
                                 bounds = OrderedDict()
-                            ## Datos para el ciclo
-                            tol_distance = tol_0
-                            ## Ciclo para busqueda de ejemplo adversarial
+                            ## Se crea la instancia de la red neuronal
+                            net = neural_network(n_neurons,n_layers,activation)
+                            ## Se cargan los parámetros de la red
+                            net.load_state_dict(torch.load('nn_parameters/{}_model_weights_L{}_n{}.pth'.format(activation,n_layers, n_neurons)))
+                            params = net.state_dict()
+                            ## Se filtran los parametros
+                            filtered_params = filter_params(params)
+                            ## Busqueda de ejemplo adversarial
                             print('\n===== Capas {} Neuronas {} =====\n'.format(n_layers,n_neurons))
-                            while tol_distance <= tol_f:
-                                print('Tolerancia: {} '.format(tol_distance))
-                                adv_ex = False
-                                ## Se crea el modelo de verificacion
-                                verif_model,all_vars = create_verification_model(params,bounds,activation,tol_distance,apply_softmax,image_list,target_output,real_output,exact,apply_bounds)
-                                if print_output:
-                                    verif_model.redirectOutput()
-                                else:
-                                    verif_model.hideOutput()
-                                ## Se limita el tiempo de resolucion
-                                verif_model.setParam('limits/time', int(60*minutes))
-                                ## Se optimiza el modelo en busca del ejemplo adversarial
-                                t0 = time.time()
+                            print('Tolerancia: {} '.format(tol_distance))
+                            adv_ex = False
+                            ## Se crea el modelo de verificacion
+                            verif_model,all_vars = create_verification_model(params,bounds,activation,tol_distance,apply_softmax,image_list,target_output,real_output,exact,apply_bounds)
+                            if print_output:
+                                verif_model.redirectOutput()
+                            else:
+                                verif_model.hideOutput()
+                            ## Se limita el tiempo de resolucion
+                            verif_model.setParam('limits/time', int(60*minutes))
+                            ## Se optimiza el modelo en busca del ejemplo adversarial
+                            t0 = time.time()
+                            try:
+                                aux_t = time.time()
+                                verif_model.optimize()
+                                dt = time.time() - aux_t
+                                model_status = verif_model.getStatus()
+                            except:
+                                dt = time.time() - t0
+                                model_status = 'problem'
+                            ## Caso solucion optima
+                            if model_status == 'optimal':
+                                gap = 0.0
+                                solution = [verif_model.getVal(all_vars['h{},{}'.format(-1,i)]) for i in range(len(image_list))]
+                                obj_val  = verif_model.getObjVal()
+                                if obj_val > 0:
+                                    ## Se encontro un ejemplo adversarial
+                                    adv_ex = True
+                                    ## Caso en que se debe guardar la imagen
+                                    if save_image:
+                                        color_map = 'gray'
+                                        png_name  = '{}_adv_ex.png'.format(activation) 
+                                        generate_png(solution,image_list,color_map,png_name,input_lb,input_ub)
+                                        output,soft_output = calculate_probs(net,solution)
+                                        print('\n Softmax output: ',soft_output,'\n')
+                            ## Caso no alcanzo el tiempo
+                            else:
                                 try:
-                                    aux_t = time.time()
-                                    verif_model.optimize()
-                                    dt = time.time() - aux_t
-                                    model_status = verif_model.getStatus()
-                                except:
-                                    dt = time.time() - t0
-                                    model_status = 'problem'
-                                ## Caso solucion optima
-                                if model_status == 'optimal':
-                                    gap = 0.0
-                                    solution      = [verif_model.getVal(all_vars['h{},{}'.format(-1,i)]) for i in range(len(image_list))]
-                                    obj_val       = verif_model.getObjVal()
-                                    if obj_val > 0:
-                                        ## Se encontro un ejemplo adversarial
+                                    primalb = verif_model.getDualbound()
+                                    dualb = verif_model.getPrimalbound()
+                                    gap = (primalb-dual)/np.abs(dualb)
+                                    ## Caso en que no existe ejemplo adversarial
+                                    if primalb < 0:
+                                        obj_val = '<0'
+                                    ## Caso en que si existe un ejemplo adversarial
+                                    elif dualb > 0:
+                                        obj_val = '>0'
                                         adv_ex = True
-                                        ## Caso en que se debe guardar la imagen
-                                        if save_image:
-                                            color_map = 'gray'
-                                            png_name  = '{}_adv_ex.png'.format(activation) 
-                                            generate_png(solution,image_list,color_map,png_name,input_lb,input_ub)
-                                            output,soft_output = calculate_probs(net,solution)
-                                            print('\n Softmax output: ',soft_output,'\n')
-                                ## Caso no alcanzo el tiempo
-                                else:
-                                    try:
-                                        primalb = verif_model.getDualbound()
-                                        dualb = verif_model.getPrimalbound()
-                                        gap = (primalb-dual)/np.abs(dualb)
-                                        ## Caso en que no existe ejemplo adversarial
-                                        if primalb < 0:
-                                            obj_val = '<0'
-                                        ## Caso en que si existe un ejemplo adversarial
-                                        elif dualb > 0:
-                                            obj_val = '>0'
-                                            adv_ex = True
-                                    except:
-                                        gap = '-'
-                                        obj_val = '-'
-                                if save_results:
-                                    ## Se lee el df existente o se genera uno nuevo
-                                    df = read_df(file_name)
-                                    ## Se genera la nueva linea del df
-                                    adv_aux = 'No'
-                                    if adv_ex:
-                                        adv_aux = 'Si'
-                                    new_line = [n_layers,n_neurons,type_bounds,tol_distance,dt,gap,adv_aux,obj_val,model_status]
-                                    ## Se añade la linea al df
-                                    df = df._append(pd.Series(new_line), ignore_index=True)
-                                    ## Se intenta guardar el nuevo df actualizado
-                                    save_df(df,file_name)
-                                ## Se aumenta la tolerancia
-                                tol_distance += tol_step
-                        else:
-                            print('error')
+                                except:
+                                    gap = '-'
+                                    obj_val = '-'
+                            if save_results:
+                                ## Se lee el df existente o se genera uno nuevo
+                                df = read_df(file_name)
+                                ## Se genera la nueva linea del df
+                                adv_aux = 'No'
+                                if adv_ex:
+                                    adv_aux = 'Si'
+                                new_line += [type_bounds,dt,gap,adv_aux,model_status]
+                    ## Se añade la linea al df
+                    df = df._append(pd.Series(new_line), ignore_index=True)
+                    ## Se intenta guardar el nuevo df actualizado
+                    save_df(df,file_name)            
