9df261a4f909b2f5b25dd1359e0f535ebda236f1
PabloC-c <pablo.carrasco@pregrado.uoh.cl>
2023-12-19 09:52:26 -0300

    files updated

--- testeo.py
+++ testeo.py
@@ -1,36 +1,12 @@
 from functions import *
 
 activation = 'sigmoid'
-exact = 'exact'
-apply_bounds = True
-type_bounds = 'verif_bounds'
-n_layers = 4
-n_neurons = 10
-real_output = 1
-target_output = 2
-tol_distance = 0.05
 
-l = 2
-i = 6
-
-if type_bounds == '-':
-    lp_sol_file = 'sols/{}_{}_{}_sol_L{}_n{}_1como{}_tolper{}.txt'.format(activation,exact,'unbound',n_layers,n_neurons,target_output,int(100*tol_distance))
-else:
-    lp_sol_file = 'sols/{}_{}_{}_sol_L{}_n{}_1como{}_tolper{}.txt'.format(activation,exact,type_bounds,n_layers,n_neurons,target_output,int(100*tol_distance))
-
-bounds_file = calculate_bounds_file_name(type_bounds,activation,n_layers,n_neurons,tol_distance,real_output)
-bounds = read_bounds(apply_bounds,n_layers,n_neurons,activation,bounds_file)
-
-points_list = get_multidim_env_points(l,bounds,activation)
-
-net = neural_network(n_neurons,n_layers,activation)
-net.load_state_dict(torch.load('nn_parameters/{}_model_weights_L{}_n{}.pth'.format(activation,n_layers, n_neurons)))
-filtered_params = filter_params(net.state_dict())
-
-n_input = len(bounds[l-1])
-
-lp_sol = generate_hyperplane_model_lpsol(l,i,n_input,activation,lp_sol_file)
-
-hp_model,c,d = create_hyperplane_model(l,i,filtered_params,bounds,lp_sol,points_list,activation)
-
-succes,cc_or_cv,c,d = calculate_hyperplane(l,i,bounds,activation,filtered_params,n_input,lp_sol_file)
+vectorlb = [-1,-1]
+vectorub = [2,2]
+w = [2,0.5]
+b = 1
+bounds = (-(b+sum(w[i]*vectorlb[i] if w[i] >= 0 else w[i]*vectorub[i]  for i in range(len(w)))),b+sum(w[i]*vectorub[i] if w[i] >=0 else w[i]*vectorlb[i] for i in range(len(w))))
+activ = get_activ_func(activation)
+print(bounds)
+cc = calculate_cc_point(activation,bounds)
